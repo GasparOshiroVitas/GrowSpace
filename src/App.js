95576ce79acb86971aec53a01741b8cdab3f413d
Gaspar Oshiro Vitas <gasparoshirovitas8@gmail.com>
2022-05-07 15:10:25 -0300

    Count

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 function App() {
+  const onAdd =(cant) =>{
+    console.log(`Agregaste ${cant} de productos`)
+  }
   return (
     <div className='App'> 
     <NavBar/>
     <ItemListContainer listadeproductos={'Productos'}/>
+    <ItemCount onAdd={onAdd}/>
     </div>
   );
 }
